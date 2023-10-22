693ecc9c6f92006512108e862a32be2b184a9089
DevVali <90271108+DevVali@users.noreply.github.com>
2023-10-22 21:46:23 +0200

    feat: add new routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Controllers\Home;
 use Lemon\Route;
 
-Route::get("/home", [Home::class, "handle"]);
+Route::redirect("/home", "/");
+
+Route::get("/", [Home::class, "handle"]);
+Route::get("/projects", [Projects::class, "handle"]);
+Route::get("/views", [Views::class, "handle"]);
 
 Route::template("/about", "about");
