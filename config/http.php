a73a008aee81c00ab65dd9cb29f8e4d147872f7e
DevVali <90271108+DevVali@users.noreply.github.com>
2023-10-22 22:06:35 +0200

    config: add more config files

---
+++ config/http.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'cors' => [
+        'alowed-origins' => '*',
+        'expose-headers' => null,
+        'max-age' => null,
+        'allowed-credentials' => null,
+        'alowed-methods' => null,
+        'allowed-headers' => null,
+    ],
+    'session' => [
+        'name' => 'PHP_SESSION',
+    ],
+];
