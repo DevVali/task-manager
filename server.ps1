37747af3037116a144974a6f16f3c9e57ff9da48
DevVali <90271108+DevVali@users.noreply.github.com>
2023-10-24 20:52:30 +0200

    fix: fix host displaying https instead of http

--- server.ps1
+++ server.ps1
@@ -1,5 +1,5 @@
 # Starts server with mix watch
 
-Write-Host "Dev server started at https://localhost:8000" -ForegroundColor yellow
+Write-Host "Dev server started at http://localhost:8000" -ForegroundColor yellow
 php -S localhost:8000 -t public &
 yarn run mix watch &
