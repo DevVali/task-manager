7ce399be7f6653677239e5e7c2e132e286b04a3d
DevVali <90271108+DevVali@users.noreply.github.com>
2023-11-11 18:40:23 +0100

    refactor: rewrite script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,71 +1,75 @@
-const menuClosed = document.querySelector("#menu-closed").classList;
-const menuOpen = document.querySelector("#menu-open").classList;
-const userMenu = document.querySelector("#user-menu").classList;
-const mobileMenu = document.querySelector("#mobile-menu").classList;
+"use strict";
 
-let isProfileDropdownVisible = false;
 let isMobileMenuVisible = false;
+let isProfileDropdownVisible = false;
 let isOptionsMenuVisible = false;
 let lastOptionsMenu;
 
-function toggleProfileDropdown() {
-	isProfileDropdownVisible = !isProfileDropdownVisible;
-
-	if (isProfileDropdownVisible === false) {
-		userMenu.remove("block");
-		userMenu.add("hidden");
-	} else {
-		userMenu.remove("hidden");
-		userMenu.add("block");
-	}
-}
+const mobileMenu = document.querySelector("#mobile-menu").classList;
+const menuClosed = document.querySelector("#menu-closed").classList;
+const menuOpen = document.querySelector("#menu-open").classList;
+const userMenu = document.querySelector("#user-menu").classList;
 
 function toggleMobileMenuDropdown() {
-	isMobileMenuVisible = !isMobileMenuVisible;
+    isMobileMenuVisible = !isMobileMenuVisible;
 
-	if (isMobileMenuVisible === false) {
-		menuClosed.remove("hidden");
-		menuClosed.add("block");
-		menuOpen.remove("block");
-		menuOpen.add("hidden");
+    if (isMobileMenuVisible === false) {
+        menuClosed.remove("hidden");
+        menuClosed.add("block");
+        menuOpen.remove("block");
+        menuOpen.add("hidden");
 
-		mobileMenu.remove("block");
-		mobileMenu.add("hidden");
-	} else {
-		menuClosed.remove("block");
-		menuClosed.add("hidden");
-		menuOpen.remove("hidden");
-		menuOpen.add("block");
+        mobileMenu.remove("block");
+        mobileMenu.add("hidden");
+    } else {
+        menuClosed.remove("block");
+        menuClosed.add("hidden");
+        menuOpen.remove("hidden");
+        menuOpen.add("block");
 
-		mobileMenu.remove("hidden");
-		mobileMenu.add("block");
-	}
+        mobileMenu.remove("hidden");
+        mobileMenu.add("block");
+    }
+}
+
+function toggleProfileDropdown() {
+    isProfileDropdownVisible = !isProfileDropdownVisible;
+
+    if (isProfileDropdownVisible === false) {
+        userMenu.remove("block");
+        userMenu.add("hidden");
+    } else {
+        userMenu.remove("hidden");
+        userMenu.add("block");
+    }
 }
 
 function toggleOptionsMenu(e) {
-	isOptionsMenuVisible = !isOptionsMenuVisible;
-	const targetMenu = document.querySelector(`#${e.target.parentNode.id.replace("-button", "")}`).classList;
+    const target = document.querySelector(`#${e.target.parentNode.id.replace("-button", "")}`).classList;
+    isOptionsMenuVisible = !isOptionsMenuVisible;
 
-	document.querySelectorAll(".card-options.block").forEach(element => {
-		element.classList.remove("block");
-		element.classList.add("hidden");
-	});
+    document.querySelectorAll(".card-options.block, #user-menu.block").forEach(element => {
+        element.classList.remove("block");
+        element.classList.add("hidden");
+    });
 
-	if (lastOptionsMenu !== targetMenu) {
-		isOptionsMenuVisible = true;
-	}
+    if (lastOptionsMenu !== target) {
+        isOptionsMenuVisible = true;
+    }
 
-	if (isOptionsMenuVisible === false) {
-		targetMenu.remove("block");
-		targetMenu.add("hidden");
-	} else {
-		targetMenu.remove("hidden");
-		targetMenu.add("block");
-	}
+    if (isOptionsMenuVisible === false) {
+        target.remove("block");
+        target.add("hidden");
+    } else {
+        target.remove("hidden");
+        target.add("block");
+    }
 
-	lastOptionsMenu = targetMenu;
+    lastOptionsMenu = target;
 }
 
-document.querySelector("#user-menu-button").addEventListener("click", toggleProfileDropdown);
 document.querySelector("#main-menu-button").addEventListener("click", toggleMobileMenuDropdown);
-document.querySelectorAll("[id*='projects-options-button-'], [id*='views-options-button-']").forEach(element => { element.addEventListener("click", toggleOptionsMenu) });
+document.querySelector("#user-menu-button").addEventListener("click", toggleProfileDropdown);
+document.querySelectorAll("[id*='projects-options-button-'], [id*='views-options-button-']").forEach(element => {
+    element.addEventListener("click", toggleOptionsMenu);
+});
